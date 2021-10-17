0a5d620418cce9d2c8959a759bfed5ac15cd4d8f
Gisilbek <gisilbek.parra@aicore.com>
2021-10-16 20:56:25 -0400

    cambiando routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       root 'home#index'
-      resources :products
-      resources :variants
+      get 'products', to: 'products#index'
+      post 'products', to: 'products#create'
+      # get "variants", to: 'variants#index'
+      post 'variants', to: 'variants#create'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
